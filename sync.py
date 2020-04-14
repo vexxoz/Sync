fdd9666e04de9b59ae197c7ffa4edb7c963765eb
Blake Caldwell <blakecaldwell123@gmail.com>
2020-04-14 16:29:48 -0700

    Added ignore file
    - File names in the ignore file of the source path will not be synced

--- sync.py
+++ sync.py
@@ -4,19 +4,26 @@ import time # sleep command
 
 fromDir = "C:/wamp64/www/SalonMaison" # from directory
 toDir = "C:/Users/blake/Documents/GitHub/Database" # directory copy of fromDir
+ingnoreFilePath = fromDir + "/ignore.txt"
 
 #how often to sync in seconds
 seconds = 90
 
+#intialize count
 count = 0
+
+#open ignore list file
+ingnoreFile = open(ingnoreFilePath, "r")
 
 #inf loop to keep syncing files
 while True:
     #for each file in the from directory
     for filename in os.listdir(fromDir):
-        #print the filename
-        shutil.copyfile((fromDir + "/" + str(filename)), (toDir + "/" + str(filename)))
-        count += 1
+        #check if file is ignored
+        if filename not in ingnoreFile.read():
+            #print the filename
+            shutil.copyfile((fromDir + "/" + str(filename)), (toDir + "/" + str(filename)))
+            count += 1
 
     print("Synced " + str(count) + " files!")
 
